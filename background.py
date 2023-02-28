445dd630d3b7de44f3d6f42f3fa7f7c5aa2ef19f
B1t3r <105979741+B1t3r@users.noreply.github.com>
2023-02-28 13:34:49 +0100

    Create background.py

---
+++ background.py
@@ -0,0 +1,8 @@
+import pygame
+from utilities.resources import get_resource
+
+MOUNTAINS = pygame.image.load(get_resource("mountains.png"))
+
+
+def draw_background(screen):
+    screen.blit(MOUNTAINS, (-500, -50))
